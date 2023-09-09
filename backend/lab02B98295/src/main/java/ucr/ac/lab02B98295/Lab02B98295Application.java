9ffcc7042a7b5da4ebae8c2eb3189f811ab1104e
Jesus Vargas <jesusvase@hotmail.com>
2023-09-09 10:43:58 -0600

    Proyecto - configuracion

---
+++ backend/lab02B98295/src/main/java/ucr/ac/lab02B98295/Lab02B98295Application.java
@@ -0,0 +1,13 @@
+package ucr.ac.lab02B98295;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab02B98295Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab02B98295Application.class, args);
+	}
+
+}
---
+++ backend/lab02B98295/src/test/java/ucr/ac/lab02B98295/Lab02B98295ApplicationTests.java
@@ -0,0 +1,13 @@
+package ucr.ac.lab02B98295;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab02B98295ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
